4d785024eb5ba539db17c765e628de036dc1adfa
iimpact75 <iimpact75@gmail.com>
2019-10-11 20:29:45 -0700

    week 4 initial push

--- js/updatednow.js
+++ js/updatednow.js
@@ -1,2 +1,2 @@
-const options = {weeday: 'long', day: 'numeric', month: 'numeric', year: 'numeric', hours: 'numeric', minutes: 'numeric', seconds: 'numeric'};
-document.getElementById('updatednow').textContent = new Date(document.lastModified).toLocaleString(options);
+const options = {weekday: 'long', day: 'numeric', month: 'numeric', year: 'numeric', hours: 'numeric', minutes: 'numeric', seconds: 'numeric'};
+document.getElementById('updatednow').textContent = new Date(document.lastModified).toLocaleString('en-US',options);

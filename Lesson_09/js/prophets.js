d2809cd9e03e4ddf2c19037894037b5d89dfe99c
iimpact75 <iimpact75@gmail.com>
2019-11-16 08:50:18 -0800

    Upload learning how to use the JS better

--- Lesson_09/js/prophets.js
+++ Lesson_09/js/prophets.js
@@ -1,12 +1,9 @@
-const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
-fetch(requestURL)
+fetch('https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json')
     .then(function (response) {
         return response.json();
     })
     .then(function (jsonObject) {
-            console.table(jsonObject); // temporary checking for valid response and data parsing
-
-
+           
             const prophets = jsonObject['prophets'];
             for (let i = 0; i < prophets.length; i++) {
                 let card = document.createElement('section');
